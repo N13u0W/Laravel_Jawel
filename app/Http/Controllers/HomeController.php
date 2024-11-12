441e8719d2fcc1a3511ac1c6c19c5acee2f4fc4f
wafaabdullah4 <menerang12345678@gmail.com>
2024-11-12 14:28:36 +0700

    first commit

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+
+{
+    function index(){
+
+    return('welcome');
+
+    }
+    function login(){
+
+    return('login');
+
+    }
+
+}
